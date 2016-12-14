9ea95f83f77f06816ff0972d64cef0a642192bfc
jgrom <jgromdev@gmail.com>
2016-12-14 15:46:28 +0100

    ~ modifs

--- public/angular-app/app.js
+++ public/angular-app/app.js
@@ -1,1 +1,20 @@
-angular.module('zapp', []);
+var app = angular.module('zapp', ['ngRoute']);
+
+app.config(function($routeProvider) {
+
+  $routeProvider
+    .when('/', {
+      templateUrl: 'angular-app/partials/main.html',
+    })
+    .otherwise({
+      redirectTo:'/'
+    });
+});
+
+
+app.directive("w3TestDirective", function() {
+    return {
+        restrict : "A",
+        template : "<h1>Made by a directive!</h1>"
+    };
+});
